b09f3d2e2a98b576227706108e0c955a12dfd39c
Jean Carlos soncco lupa <jsonccolu@unsa.edu.pe>
2021-12-30 09:54:12 -0500

    update credentials

--- backend/videosearch/views.py
+++ backend/videosearch/views.py
@@ -12,7 +12,7 @@ from google.cloud import storage, bigquery
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
 
-    storage_client = storage.Client() #.from_service_account_json('cred.json')
+    storage_client = storage.Client.from_service_account_json('videosearch/cred.json')
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
@@ -27,7 +27,7 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
 
 def list_blobs_with_prefix(bucket_name, prefix, delimiter=None):
-    storage_client = storage.Client()
+    storage_client = storage.Client.from_service_account_json('videosearch/cred.json')
     # Note: Client.list_blobs requires at least package version 1.17.0.
     blobs = storage_client.list_blobs(bucket_name, prefix=prefix, delimiter=delimiter)
 
@@ -66,7 +66,7 @@ class FilterVideoAPIview(APIView):
 
     def get(self, request, *args, **kwargs):
         # Construct a BigQuery client object.
-        client = bigquery.Client()
+        client = bigquery.Client.from_service_account_json('videosearch/cred.json')
 
         lookup = self.kwargs['query']
 
@@ -79,7 +79,7 @@ class FilterVideoAPIview(APIView):
         for row in query_job:
             data.append({'name': row[1], 
                             'etiqueta':row[0],
-                            'url_video': 'storage.cloud.google.com/videosearch/'+row[1]+'.mp4',
-                            'url_gif': 'storage.cloud.google.com/video-gifs/'+row[1]+'.gif',
-                            'url_image':'storage.cloud.google.com/video-thumbs/'+row[1]+'.png'})
+                            'url_video': 'storage.googleapis.com/videosearch/'+row[1]+'.mp4',
+                            'url_gif': 'storage.cloud.google.com/video-gifs/'+row[1],
+                            'url_image':'storage.cloud.google.com/video-thumbs/'+row[1]})
         return Response(data, status=status.HTTP_200_OK)
